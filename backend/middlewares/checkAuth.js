81aa77188e8bc5a7ac07d0dea90eb6791b031c28
MinhajSadik <MinhajoSadik@gmail.com>
2022-09-09 14:30:00 +0600

    [CHANGED] some new things

--- backend/middlewares/checkAuth.js
+++ backend/middlewares/checkAuth.js
@@ -3,8 +3,8 @@ import User from "../models/userModel.js";
 import ErrorHandler from "../utils/errorHandler.js";
 
 export const checkAuth = async (req, res, next) => {
-  // const token = req.headers.authorization.split(" ")[1];
-  const token = req.headers;
+  const token = req.headers.authorization.split(" ")[1];
+  // const token = req.headers;
 
   if (!token) {
     return next(new ErrorHandler("Please login to access this resource", 401));
