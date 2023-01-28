17e60a7f4c955a85535caa64e061c7665c6e00d0
MinhajSadik <MinhajoSadik@gmail.com>
2023-01-28 12:59:20 +0600

    [RE-CHECK] api do not working

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -40,6 +40,8 @@ const userController = {
       res.cookie("token", token, {
         maxAge: 1000 * 60 * 60 * 24,
         httpOnly: true,
+        sameSite: "none",
+        secure: true
       });
 
 
